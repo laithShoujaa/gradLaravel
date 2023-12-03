15b57b49e10de125cdf4a0d7ee5aa4929795820b
LaithHaithamAzzam <laethazzam.la.la@gmail.com>
2023-12-03 23:38:33 +0300

    fileApis

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -20,9 +20,55 @@ class FilesController extends Controller
       ]);
       $file = Files::where('id', $request['id'])->first();
       if ($file != null) {
-        $responseFile = Storage::disk('public')->get($file->path);
-        return (new Response($responseFile, 200))->header('Content-Type', $file->typeFile);
+        $responseFile = Storage::disk('public')->get($file['filePath']);
+        return (new Response($responseFile, 200))->header('Content-Type', $file['fileType']);
       }
+    } catch (Exception $e) {
+      return response()->json([
+        "state" => false,
+        "data" => $e->getMessage()
+      ]);
+    }
+  }
+
+  public function getCardFile(Request $request)
+  {
+    try {
+      $request->validate([
+        'passcode' => 'required',
+        'userId' => 'required'
+      ]);
+      $data = Files::where('userId', $request->userId)
+        ->where('passcode', $request->passcode)
+        ->where('drugSens', false)
+        ->first(['id', 'detail', 'fileName']);
+      return response()->json([
+        'state' => true,
+        'data' => $data
+      ]);
+    } catch (Exception $e) {
+      return response()->json([
+        "state" => false,
+        "data" => $e->getMessage()
+      ]);
+    }
+  }
+
+  public function getCardDrugSens(Request $request)
+  {
+    try {
+      $request->validate([
+        'passcode' => 'required',
+        'userId' => 'required'
+      ]);
+      $data = Files::where('userId', $request->userId)
+        ->where('passcode', $request->passcode)
+        ->where('drugSens', true)
+        ->first(['id', 'detail', 'fileName']);
+      return response()->json([
+        'state' => true,
+        'data' => $data
+      ]);
     } catch (Exception $e) {
       return response()->json([
         "state" => false,
