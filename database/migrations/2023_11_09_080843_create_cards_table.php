3fcba3462b8f9ddbd05f2a9baaae01e4180108b9
LaithHaithamAzzam <laetheazzam.la.la@gmail.com>
2023-12-07 22:42:25 +0200

    fix

--- database/migrations/2023_11_09_080843_create_cards_table.php
+++ database/migrations/2023_11_09_080843_create_cards_table.php
@@ -15,7 +15,7 @@ class CreateCardsTable extends Migration {
 			$table->enum('typeCard', array('nfc', 'smartHome'));
 			$table->string('name');
 			$table->date('birthDate')->nullable();
-			$table->enum('gender', array('male', 'femal'))->nullable();
+			$table->enum('gender', array('male', 'female'))->nullable();
 			$table->string('location')->nullable();
             $table->enum('blood',['AB+','AB-','A+','A-','B+','B-','O+','O-'])->nullable();
 			$table->string('phone')->nullable();
